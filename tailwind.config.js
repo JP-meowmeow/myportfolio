e5570124c997e8204ff4097e103934948f39a697
JP-meowmeow <jp.jirapat.t@gmail.com>
2025-02-02 15:45:31 +0700

    test

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,4 +1,5 @@
 /** @type {import('tailwindcss').Config} */
+
 export default {
   content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
@@ -19,7 +20,7 @@ export default {
     },
   },
   plugins: [
-    require("daisyui"),
+    import("daisyui"),
     function ({ addUtilities }) {
       addUtilities({
         ".text-shadow-sm": { textShadow: "1px 1px 2px rgba(0, 0, 0, 0.25)" },
